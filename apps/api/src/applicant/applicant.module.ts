9d705d1e5d1f477e317f1adcb4a3914d8b60f91b
Bhumin Patel <bhumin@freshworks.io>
2022-02-24 01:09:44 +0530

    Fix import file name

--- apps/api/src/applicant/applicant.module.ts
+++ apps/api/src/applicant/applicant.module.ts
@@ -4,7 +4,7 @@ import { MailModule } from 'src/mail/mail.module';
 import { ApplicantEntity } from './entity/applicant.entity';
 import { ApplicantStatusEntity } from './entity/applicantStatus.entity';
 import { ApplicantController } from './applicant.controller';
-import { ApplicantStatusController } from './applicantstatus.controller';
+import { ApplicantStatusController } from './applicantStatus.controller';
 import { ApplicantService } from './applicant.service';
 import { ApplicantStatusService } from './applicantStatus.service';
 
