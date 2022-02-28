a7734d567ef79cdc21425bb55081fd5087022cb9
Bhumin Patel <bhumin@freshworks.io>
2022-02-28 17:37:56 +0530

    Lint issue fixed

--- packages/common/src/dto/applicant-update.dto.ts
+++ packages/common/src/dto/applicant-update.dto.ts
@@ -1,94 +1,93 @@
 import {
-    IsDateString,
-    IsNotEmpty,
-    IsOptional,
-    IsString,
-    Length,
-    IsObject,
-    IsBoolean,
-    IsInt,
-  } from 'class-validator';
-  
-  export class ApplicantUpdateDto {
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    @Length(1, 256)
-    first_name?: string;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    last_name?: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    @Length(1, 256)
-    profession?: string;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    speciality?: string;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    assigned_to?: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    @Length(1, 256)
-    ha_pcn?: string;
-  
-    @IsInt()
-    @IsOptional()
-    status?: number;
-  
-    @IsDateString()
-    @IsOptional()
-    first_referral?: Date;
-  
-    @IsDateString()
-    @IsOptional()
-    latest_referral?: Date;
-  
-    @IsDateString()
-    @IsOptional()
-    followed_up?: Date;
-  
-    @IsDateString()
-    @IsOptional()
-    date_matched?: Date;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    comment?: string;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    added_by?: string;
-  
-    @IsString()
-    @Length(1, 256)
-    @IsOptional()
-    added_by_id?: string;
-  
-    @IsBoolean()
-    @IsOptional()
-    is_open?: boolean;
-  
-    @IsObject()
-    @IsOptional()
-    additional_data?: JSON;
-  
-    // Use this field when status start date is not current date
-    @IsDateString()
-    @IsOptional()
-    status_date?: Date;
-  }
-  
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  IsObject,
+  IsBoolean,
+  IsInt,
+} from 'class-validator';
+
+export class ApplicantUpdateDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @Length(1, 256)
+  first_name?: string;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  last_name?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @Length(1, 256)
+  profession?: string;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  speciality?: string;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  assigned_to?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @Length(1, 256)
+  ha_pcn?: string;
+
+  @IsInt()
+  @IsOptional()
+  status?: number;
+
+  @IsDateString()
+  @IsOptional()
+  first_referral?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  latest_referral?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  followed_up?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  date_matched?: Date;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  comment?: string;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  added_by?: string;
+
+  @IsString()
+  @Length(1, 256)
+  @IsOptional()
+  added_by_id?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  is_open?: boolean;
+
+  @IsObject()
+  @IsOptional()
+  additional_data?: JSON;
+
+  // Use this field when status start date is not current date
+  @IsDateString()
+  @IsOptional()
+  status_date?: Date;
+}
