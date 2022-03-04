16072239d9c492d45bbddc9d63397c849044e2b3
David Bayly <dbayly@freshworks.io>
2022-03-04 08:08:47 -0800

    FE work mostly complete. Adding user entity and related migration to the back end.

---
+++ apps/api/src/migration/1646343985965-AddEmployee.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddEmployee1646343985965 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
