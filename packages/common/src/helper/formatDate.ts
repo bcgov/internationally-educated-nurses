f7e90e4dfcb8f55c70ff6f42bbd426b2dec58521
Cory Atwater <cory@freshworks.io>
2022-03-29 16:45:19 -0400

    refactor date formatter

--- packages/common/src/helper/formatDate.ts
+++ packages/common/src/helper/formatDate.ts
@@ -1,15 +1,11 @@
+import dayjs from 'dayjs';
+
 export const formatDate = (value: string) => {
   if (!value) {
     return;
   }
 
-  const date = new Date(value);
+  const formattedDate = dayjs(value).format('MMM DD, YYYY');
 
-  // Date object subtracts a day, so need to re-add by 1
-  const day = parseInt(date.toLocaleString('default', { day: '2-digit' })) + 1;
-
-  const month = date.toLocaleString('default', { month: 'short' });
-
-  const year = date.toLocaleString('default', { year: 'numeric' });
-  return `${month} ${day}, ${year}`;
+  return formattedDate;
 };
