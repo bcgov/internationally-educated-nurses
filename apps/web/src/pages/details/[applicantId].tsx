119289c1def6de8dce99af38d41487e1772426a5
Sunghwan Park <95253983+sunghwan-park-fw@users.noreply.github.com>
2022-04-14 08:31:26 -0700

    IEN-218 | style details page (#56)

--- apps/web/src/pages/details/[applicantId].tsx
+++ apps/web/src/pages/details/[applicantId].tsx
@@ -1,5 +1,3 @@
-import { faClock, faListAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -9,6 +7,8 @@ import { Recruitment } from 'src/components/milestone-logs/Recruitment';
 import { DetailsItem } from '@components';
 import { ApplicantRO, formatDate } from '@ien/common';
 import { Spinner } from 'src/components/Spinner';
+import detailIcon from '@assets/img/details.svg';
+import historyIcon from '@assets/img/history.svg';
 
 const Details = () => {
   const [applicant, setApplicant] = useState<ApplicantRO>();
@@ -47,35 +47,34 @@ const Details = () => {
   ];
 
   return (
-    <div>
+    <div className='container w-full mx-6 xl:w-xl mb-4 px-4'>
       <p className='text-xs text-gray-400 mt-3 mb-5'>
         Manage Applicants / <span className='text-blue-800 font-bold'>Applicant Details</span>
       </p>
       <h1 className='font-bold text-3xl'>
         {applicant.name} #{applicant.applicant_id ? applicant.applicant_id : 'NA'}
       </h1>
-      <p className='text-gray-400 text-sm pt-1 pb-4'>Last Updated: January 5, 2022</p>
+      <p className='text-gray-400 text-sm pt-1 pb-4'>
+        Last Updated: {formatDate(applicant.updated_date)}
+      </p>
       {/* Details container */}
-      <div className='grid grid-cols-12 border-2 rounded px-5 pb-3 bg-white'>
+      <div className='grid grid-cols-12 gap-2 border-2 rounded px-5 pb-3 bg-white text-bcBlack'>
         <div className='col-span-12 border-b mb-3'>
-          <h1 className='text-xl text-blue-900 py-3 font-bold'>
-            <FontAwesomeIcon
-              className='h-5 mr-2 inline-block items-center'
-              icon={faListAlt}
-            ></FontAwesomeIcon>
-            Details
-          </h1>
+          <div className='flex flex-row align-bottom py-4 font-bold'>
+            <img src={detailIcon.src} alt='detail icon' />
+            <h2 className='ml-2 font-bold text-xl text-bcBluePrimary'>Details</h2>
+          </div>
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Email Address' text={applicant.email_address} />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Phone Number' text={applicant.phone_number} />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Registration date' text={formatDate(applicant.registration_date)} />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem
             title='Assigned To'
             text={
@@ -87,22 +86,22 @@ const Details = () => {
             }
           />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem
             title='Country of Citizenship'
             text={applicant.country_of_citizenship?.join(', ')}
           />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Country of Residence' text={applicant.country_of_residence} />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Permanent Resident Status' text={applicant.pr_status} />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='Country of Nursing Education' text='Needs to remove it' />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem
             title='Nursing Education'
             text={
@@ -113,16 +112,16 @@ const Details = () => {
             }
           />
         </div>
-        <div className='col-span-3'>
+        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
           <DetailsItem title='BCCNM License Number' text={applicant.bccnm_license_number} />
         </div>
       </div>
 
       {/* Milestones logs container */}
       <div className='border-2 rounded px-5 my-5 bg-white'>
-        <div className='flex items-center border-b'>
-          <FontAwesomeIcon className='h-5 mr-2 text-blue-900 ' icon={faClock}></FontAwesomeIcon>
-          <h1 className='text-xl text-blue-900 py-4 font-bold'>Milestones Logs</h1>
+        <div className='flex items-center border-b py-4'>
+          <img src={historyIcon.src} alt='history icon' />
+          <h2 className='ml-2 font-bold text-bcBluePrimary text-xl'>Milestones Logs</h2>
         </div>
         <HeaderTab tabs={milestoneTabs} tabIndex={currentTab} onTabClick={setCurrentTab} />
         {logType[currentTab - 1]?.component}
