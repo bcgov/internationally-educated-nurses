8a49fc3f12cb170c185c8a09e7daf4b8bd705e7e
Cory Atwater <cory@freshworks.io>
2022-04-21 14:44:59 -0400

    remove unneeded div

--- apps/web/src/components/MenuBar.tsx
+++ apps/web/src/components/MenuBar.tsx
@@ -14,16 +14,15 @@ export const MenuBar: React.FC = () => {
       <div className='w-full 2xl:w-5/6 h-full flex flex-row items-center align-center justify-between px-2 md:px-12'>
         <div className='layout-grid gap-0 h-full flex flex-row items-center align-center'>
           {menuBarTabs.map(({ title, path }) => (
-            <div
-              key={title}
-              className={`text-white text-sm py-2 pr-12 ${
-                router.pathname == path ? active : 'pointer-events-none'
-              }`}
-            >
-              <Link href={path}>
-                <a>{title}</a>
-              </Link>
-            </div>
+            <Link key={title} href={path}>
+              <a
+                className={`text-white text-sm py-2 pr-12 ${
+                  router.pathname == path ? active : 'pointer-events-none'
+                }`}
+              >
+                {title}
+              </a>
+            </Link>
           ))}
         </div>
       </div>
