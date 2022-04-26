01a8da8133432ec8144eedd6d51141d44bf9abcc
Sunghwan Park <sunghwan@freshworks.io>
2022-04-26 15:49:04 -0700

    rename js to ts

--- apps/web/cypress/plugins/index.ts
+++ apps/web/cypress/plugins/index.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example plugins/index.ts can be used to load plugins
 //
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
@@ -13,10 +13,13 @@
 // the project's config changing)
 
 /**
+ * @param on
+ * @param config
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+// eslint-disable-next-line no-unused-vars,
+// @typescript-eslint/no-unused-vars
+module.exports = () => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
