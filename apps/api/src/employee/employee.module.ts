882bd9c04c0024219a9beb9f305e14e1f46714a7
bhumin-fw <99269796+bhumin-fw@users.noreply.github.com>
2022-04-26 21:10:25 +0530

    IEN-250 added API update user role (#84)

--- apps/api/src/employee/employee.module.ts
+++ apps/api/src/employee/employee.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { EmployeeController } from './employee.controller';
 import { EmployeeEntity } from './employee.entity';
 import { EmployeeService } from './employee.service';
 
 @Module({
   controllers: [EmployeeController],
-  imports: [TypeOrmModule.forFeature([EmployeeEntity])],
+  imports: [TypeOrmModule.forFeature([EmployeeEntity]), forwardRef(() => AuthModule)],
   providers: [EmployeeService],
   exports: [EmployeeService],
 })
--- apps/api/src/employee/employee.service.ts
+++ apps/api/src/employee/employee.service.ts
@@ -1,5 +1,7 @@
+import { BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ValidRoles } from 'src/auth/auth.constants';
+import { FindManyOptions, ILike, In, Repository } from 'typeorm';
 import { EmployeeEntity } from './employee.entity';
 
 export class EmployeeService {
@@ -21,5 +23,49 @@ export class EmployeeService {
   }
   async getUserByKeycloakId(keycloakId: string): Promise<EmployeeEntity | undefined> {
     return await this.employeeRepository.findOne({ keycloakId });
+  }
+
+  /**
+   * List and filter employees,
+   * Only for administrator purposes
+   * We will build UI for this when we extend our scope to provide user-management
+   * @param name optional name wise filter
+   * @returns Employee/User's list
+   */
+  async getEmployeeList(name: string): Promise<EmployeeEntity[]> {
+    const query: FindManyOptions<EmployeeEntity> = {
+      select: ['id', 'name', 'role'],
+    };
+    if (name && name != '') {
+      query.where = { name: ILike(`%${name}%`) };
+    }
+    return await this.employeeRepository.find(query);
+  }
+
+  /**
+   * Update role of the provided employee list
+   * @param ids Employee Ids whose role we are updating
+   * @param role role
+   */
+  async updateRole(ids: string[], role: string): Promise<void> {
+    if (!Object.values<string>(ValidRoles).includes(role)) {
+      throw new BadRequestException(`Provided role does not exist`);
+    }
+
+    const query: FindManyOptions<EmployeeEntity> = {};
+    if (ids && ids.length > 0) {
+      query.where = { id: In(ids) };
+    } else {
+      throw new BadRequestException(`Please provide atleast one Id`);
+    }
+    const employees = await this.employeeRepository.count(query);
+    if (employees !== ids.length) {
+      throw new BadRequestException(
+        `All provided Ids does not exist, Provided ids ${ids.length}, exist on system ${employees}`,
+      );
+    }
+
+    // It's time! let's update role for the provided Ids
+    await this.employeeRepository.update(ids, { role: role });
   }
 }
