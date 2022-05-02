29b8bac8f22ee5e619b7d0fd2dc0648d27d83204
Sunghwan Park <sunghwan@freshworks.io>
2022-05-02 15:52:51 -0700

    remove unused files from ehpr

--- apps/web/src/components/submission/components/index.ts
+++
@@ -1,9 +0,0 @@
-export interface FormStepProps {
-  formKey: string;
-}
-
-export * from './Contact';
-export * from './Credential';
-export * from './Preferences';
-export * from './Personal';
-export * from './Review';
--- packages/common/src/interfaces/index.ts
+++ packages/common/src/interfaces/index.ts
@@ -1,2 +1,1 @@
-export * from './form-payload';
 export * from './job-query-options';
