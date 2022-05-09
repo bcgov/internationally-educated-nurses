11a111eacb3b802a4d9da4aae55b42a56cc44623
Cory Atwater <cory@freshworks.io>
2022-05-09 14:33:41 -0400

    remove check from specific applicant search

--- apps/web/src/pages/applicants.tsx
+++ apps/web/src/pages/applicants.tsx
@@ -58,9 +58,7 @@ const Applicants = () => {
     setLoading(false);
   }, [name, status, sortKey, order, pageIndex, limit]);
 
-  // to-do change to URLSearch params during next sprint
-  const viewDetail = (name: string, status: number | undefined, id: string) =>
-    router.push(`/details?name=${name}&status=${status}&id=${id}`);
+  const viewDetail = (id: string) => router.push(`/details?id=${id}`);
 
   const handleSort = (key: string) => {
     if (key === sortKey) {
