a88f8ad6cca3e3a794526fd9ab61a20055578a30
Bhumin Patel <bhumin@freshworks.io>
2022-05-12 19:06:09 +0530

    Revert "API integration test cases"
    This reverts commit 88ecefd66aaa4231987ebef879f058ae10c6a2cc.

--- apps/api/src/auth/auth.guard.ts
+++ apps/api/src/auth/auth.guard.ts
@@ -15,11 +15,6 @@ export class AuthGuard implements CanActivate {
     /** Grab reference to the request */
     const request = context.switchToHttp().getRequest();
 
-    if (process.env.NODE_ENV === 'test') {
-      request.user = { user_id: 1 };
-      return true;
-    }
-
     const tokenUser = await this.authService.getUserFromToken(
       this.authService.extractToken(request.headers || '') || '',
     );
