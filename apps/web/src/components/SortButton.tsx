4c04586427ab1c336bc99a251b8c95b9fef5476b
Sunghwan Park <sunghwan@freshworks.io>
2022-05-17 13:48:37 -0700

    IEN-327 | sort applicants by updated_date

---
+++ apps/web/src/components/SortButton.tsx
@@ -0,0 +1,18 @@
+import sortIcon from '@assets/img/sort.svg';
+
+export interface SortButtonProps {
+  label: string;
+  sortKey: string;
+  onChange: (key: string) => void;
+}
+
+export const SortButton = ({ label, sortKey, onChange }: SortButtonProps) => {
+  return (
+    <div className='flex align-middle justify-between'>
+      <span>{label}</span>
+      <button id='sort-by-id' onClick={() => onChange(sortKey)}>
+        <img src={sortIcon.src} alt='sort' />
+      </button>
+    </div>
+  );
+};
