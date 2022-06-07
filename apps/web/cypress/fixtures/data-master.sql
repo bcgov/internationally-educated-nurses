ee05a967fd48a8497ffe7b1b02a03785ec099423
Bhumin Patel <bhumin@freshworks.io>
2022-06-07 13:54:39 +0530

    Increase e2e test duration and update seed data for withdrawal reasons

--- apps/web/cypress/fixtures/data-master.sql
+++ apps/web/cypress/fixtures/data-master.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.ien_status_reasons (id, name) VALUES (1, 'Pay too low');
-INSERT INTO public.ien_status_reasons (id, name) VALUES (2, 'Location too far');
-INSERT INTO public.ien_status_reasons (id, name) VALUES (3, 'Not enough benefits');
-INSERT INTO public.ien_status_reasons (id, name) VALUES (4, 'Location too expensive');
+INSERT INTO public.ien_status_reasons (id, name, recruitment) VALUES (1, 'Pay too low', true);
+INSERT INTO public.ien_status_reasons (id, name, recruitment) VALUES (2, 'Location too far', true);
+INSERT INTO public.ien_status_reasons (id, name, recruitment) VALUES (3, 'Not enough benefits', true);
+INSERT INTO public.ien_status_reasons (id, name, recruitment) VALUES (4, 'Location too expensive', true);
