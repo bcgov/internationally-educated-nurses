0ad3d65090512990b845b9e0f8ac241164623f7b
Bhumin Patel <bhumin@freshworks.io>
2022-06-10 17:31:29 +0530

    Going with default batch size 10 for SQS

--- terraform/notification_SQS_Lambda.tf
+++ terraform/notification_SQS_Lambda.tf
@@ -57,5 +57,4 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.terraform_queue.arn
   enabled          = true
   function_name    = aws_lambda_function.SQSLambda.arn
-  batch_size       = 1
 }
