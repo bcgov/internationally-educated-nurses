146c168f860a7076db726d03976c214467705134
Bhumin Patel <bhumin@freshworks.io>
2022-06-10 13:24:49 +0530

    Fix SQS send message error

--- apps/api/src/common/sendToSQS.ts
+++ apps/api/src/common/sendToSQS.ts
@@ -4,24 +4,17 @@ AWS.config.update({ region: process.env.AWS_S3_REGION });
 const SQS = new AWS.SQS({ apiVersion: '2012-11-05' });
 const sqsQueueUrl = process.env.SQS_QUEUE_URL;
 
-export default function sendToSQS(data: unknown): void {
+export default async function sendToSQS(data: unknown): Promise<void> {
   if (sqsQueueUrl) {
-    // eslint-disable-next-line no-console
-    console.log('SQS function triggered, Lets send message');
+    Logger.log('SQS function triggered, Lets send message');
     const params = {
       MessageBody: JSON.stringify(data),
       QueueUrl: sqsQueueUrl,
     };
-
-    SQS.sendMessage(params, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.log({err});
-        return;
-      }
-      // eslint-disable-next-line no-console
-      console.log({result});
-    });
+    Logger.log('Sending message', 'sendToSQS');
+    const message = await SQS.sendMessage(params).promise();
+    Logger.log('message sent', 'sendToSQS');
+    Logger.log({ message });
   } else {
     Logger.warn('SQS_QUEUE_URL not available, Message not sent', 'sendToSlack');
   }
