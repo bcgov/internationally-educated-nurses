97fa2d35fa09e1b16a4ff9329fa86ab325528539
Bhumin Patel <bhumin@freshworks.io>
2022-06-10 17:36:49 +0530

    Fix batch size error

--- terraform/notification_SQS_Lambda.tf
+++ terraform/notification_SQS_Lambda.tf
@@ -57,4 +57,5 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.terraform_queue.arn
   enabled          = true
   function_name    = aws_lambda_function.SQSLambda.arn
+  batch_size       = 10
 }
