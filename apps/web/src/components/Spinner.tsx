dd079ebcd7fa9bcca1c7e7a250e249ba787fd05a
Sunghwan Park <95253983+sunghwan-park-fw@users.noreply.github.com>
2022-06-13 09:10:20 -0700

    display spinner when downloading report (#200)
    * display spinner when downloading report

--- apps/web/src/components/Spinner.tsx
+++ apps/web/src/components/Spinner.tsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-interface SPinnerProps {
+interface SpinnerProps {
   relative?: boolean;
   className?: string;
 }
 
-export const Spinner = ({ className, relative }: SPinnerProps) => {
+export const Spinner = ({ className, relative }: SpinnerProps) => {
   const classes = relative
     ? 'w-full h-full flex items-center justify-center'
     : 'fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2';
