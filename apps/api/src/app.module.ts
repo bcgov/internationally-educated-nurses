397a19dccbbf0e7df3281bc45cc14ba6d095d752
Bhumin Patel <bhumin@freshworks.io>
2022-06-15 00:40:21 +0530

    Added log for request-response with the help of middleware.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -8,10 +8,15 @@ import { FormModule } from './form/form.module';
 import { EmployeeModule } from './employee/employee.module';
 import { AuthModule } from './auth/auth.module';
 import { ReportModule } from './report/report.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [DatabaseModule, FormModule, ApplicantModule, EmployeeModule, AuthModule, ReportModule],
   controllers: [AppController],
   providers: [AppService, AppLogger],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
