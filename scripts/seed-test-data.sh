256b36f471a0956701704b17a2ea57dfeffc6290
Sunghwan Park <sunghwan@freshworks.io>
2022-06-21 16:50:26 -0700

    IEN-433 | increase wait time for api to run

--- scripts/seed-test-data.sh
+++ scripts/seed-test-data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "wait for api to be up and running"
-for i in {1..30}
+for i in {1..120}
 do
   curl http://localhost:4000/api/v1/version 2> /dev/null
   if [[ $? -eq 0 ]]
   then
     break
-  elif [[ $i -eq 30 ]]
+  elif [[ $i -eq 120 ]]
   then
     echo "[ERROR] api is not responding!"
     exit 1
