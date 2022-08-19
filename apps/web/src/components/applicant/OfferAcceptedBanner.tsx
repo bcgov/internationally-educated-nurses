37a2496f44450f48702de9ae79f4f235ffd74ecb
Sunghwan Park <sunghwan@freshworks.io>
2022-08-19 11:51:18 -0700

    IEN-628 | hide hired status from other authorities

--- apps/web/src/components/applicant/OfferAcceptedBanner.tsx
+++ apps/web/src/components/applicant/OfferAcceptedBanner.tsx
@@ -1,28 +1,19 @@
 import { Banner } from '@components';
 
 import hiredIndCheckmark from '@assets/img/hired_checkmark_banner.svg';
-import hiredIndIcon from '@assets/img/hired_indicator.svg';
 import { useApplicantContext } from './ApplicantContext';
 import { useAuthContext } from '../AuthContexts';
 
-export const OfferAcceptedBanner: React.FC = () => {
+export const OfferAcceptedBanner = () => {
   const { hiredHa } = useApplicantContext();
   const { authUser } = useAuthContext();
 
-  if (!hiredHa) {
-    return null;
-  }
-
-  if (hiredHa === authUser?.ha_pcn_id) {
+  if (hiredHa && hiredHa === authUser?.ha_pcn_id) {
     return (
       <Banner style='success' image={hiredIndCheckmark.src}>
         This applicant has already accepted your job offer and has been hired.
       </Banner>
     );
   }
-  return (
-    <Banner style='warning' image={hiredIndIcon.src}>
-      This applicant has already accepted a job offer and has been hired to another job competition.
-    </Banner>
-  );
+  return null;
 };
--- packages/common/src/helper/is-hired.ts
+++ packages/common/src/helper/is-hired.ts
@@ -1,5 +1,11 @@
 import { STATUS } from '../enum';
+import { ApplicantRO, EmployeeRO } from '../ro';
 
 export const isHired = (id?: number) => {
   return id === STATUS.Candidate_accepted_the_job_offer;
+};
+
+export const isHiredByUs = (applicant: ApplicantRO, user?: EmployeeRO): boolean => {
+  const { job_accepted } = applicant;
+  return !!job_accepted && !!user && job_accepted.ha_pcn.id === user.ha_pcn_id;
 };
