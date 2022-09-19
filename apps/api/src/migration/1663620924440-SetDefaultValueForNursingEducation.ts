42344fb3fad1c28cd4987dc1fa6ae7bce944e29a
David Bayly <dbayly@freshworks.io>
2022-09-19 14:41:33 -0700

    ran formatter

--- apps/api/src/migration/1663620924440-SetDefaultValueForNursingEducation.ts
+++ apps/api/src/migration/1663620924440-SetDefaultValueForNursingEducation.ts
@@ -1,18 +1,21 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class SetDefaultValueForNursingEducation1663620924440 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `update ien_applicants set nursing_educations = '"[]"'  WHERE nursing_educations IS NULL;`,
+    );
+    await queryRunner.changeColumn(
+      'ien_applicants',
+      'nursing_educations',
+      new TableColumn({
+        name: 'nursing_educations',
+        type: 'jsonb',
+        isNullable: false,
+        default: "'[]'",
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`update ien_applicants set nursing_educations = '"[]"'  WHERE nursing_educations IS NULL;`);
-        await queryRunner.changeColumn('ien_applicants','nursing_educations', new TableColumn({
-            name:'nursing_educations',
-            type:'jsonb',
-            isNullable:false,
-            default:"'[]'"
-        }))
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
