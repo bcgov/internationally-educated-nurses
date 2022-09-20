78505663b605e3d3aba336f51badb74e27d12d8f
Sunghwan Park <sunghwan@freshworks.io>
2022-09-20 15:23:33 -0700

    IEN-695 insert 'withdrew from competition' milestone

--- apps/api/src/migration/1663698520477-RemoveDuplicateMilestones.ts
+++ apps/api/src/migration/1663698520477-RemoveDuplicateMilestones.ts
@@ -34,6 +34,16 @@ export class RemoveDuplicateMilestones1663698520477 implements MigrationInterfac
       .delete()
       .where({ id: In(status_id_list) })
       .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder(IENApplicantStatus, 'status')
+      .insert()
+      .values({
+        id: '3fd4f2b0-5151-d7c8-6bbc-3a0601b5e1b0',
+        status: 'Candidate Withdrew from Competition',
+        category: 'IEN Recruitment Process',
+      })
+      .execute();
   }
 
   public async down(): Promise<void> {
