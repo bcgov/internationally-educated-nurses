2b3f47e8551434c9b864c83ee232534bfc8da512
Sunghwan Park <sunghwan@freshworks.io>
2022-09-20 13:27:54 -0700

    IEN-695 remove duplicate milestones

---
+++ apps/api/src/migration/1663698520477-RemoveDuplicateMilestones.ts
@@ -0,0 +1,42 @@
+import { In, MigrationInterface, QueryRunner } from 'typeorm';
+import { IENApplicantStatusAudit } from '../applicant/entity/ienapplicant-status-audit.entity';
+import { IENApplicantStatus } from '../applicant/entity/ienapplicant-status.entity';
+
+export class RemoveDuplicateMilestones1663698520477 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const status_id_list = [
+      '7B97462B-8600-41F5-AAFD-3672B38C8C85', // No position available
+      '10277492-D10A-4EBD-9F50-39F4BE9D05F7', // HA was not interested
+      '049E5172-578D-49CE-BC0D-631C4FC1965C', // Job competition cancelled
+      '009761C9-A857-4E40-978A-9CBCC6F94E35', // Candidate withdrew from the job competition
+      'E26EC530-2019-44FC-9784-D565BDD3FFDE', // Job offer accepted
+    ];
+
+    await queryRunner.manager
+      .createQueryBuilder(IENApplicantStatusAudit, 'audit')
+      .delete()
+      .where({ status: In(status_id_list) })
+      .execute();
+
+    await queryRunner.query(`
+      UPDATE ien_applicants a
+      SET status_id = (
+        SELECT status_id
+        FROM "ien_applicant_status_audit" s
+        WHERE a.id = s.applicant_id
+        ORDER BY s.start_date DESC
+        LIMIT 1
+      )
+    `);
+
+    await queryRunner.manager
+      .createQueryBuilder(IENApplicantStatus, 'status')
+      .delete()
+      .where({ id: In(status_id_list) })
+      .execute();
+  }
+
+  public async down(): Promise<void> {
+    /* no rollback */
+  }
+}
