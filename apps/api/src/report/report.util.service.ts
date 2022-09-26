6d574e2faa86d84dfec0157a6167b2940232c0fb
Sunghwan Park <95253983+sunghwan-park-fw@users.noreply.github.com>
2022-09-26 11:08:43 -0700

    IEN-710 fix report 5 - exclude inactive(withdrew, hired) applicant (#404)
    * IEN-710 fix report 5 - exclude inactive applicants
    * IEN-710 order milestone by start_date and updated_date

--- apps/api/src/report/report.util.service.ts
+++ apps/api/src/report/report.util.service.ts
@@ -266,8 +266,8 @@ export class ReportUtilService {
                   WHERE
                     ien_status.applicant_id = applicants.id AND ien_status.start_date::date <= '${to}' AND
                     status.category IN ('${StatusCategory.LICENSING_REGISTRATION}', '${StatusCategory.RECRUITMENT}')
-                  ORDER BY ien_status.start_date
-                  DESC limit 1
+                  ORDER BY ien_status.start_date DESC, ien_status.updated_date DESC
+                  LIMIT 1
                 ),
                 '${this.nil_uuid}'
               ) IN ('${StatusId.WITHDREW_FROM_PROGRAM}', '${StatusId.JOB_OFFER_ACCEPTED}') 
@@ -337,37 +337,71 @@ export class ReportUtilService {
 
   licenseApplicantsQuery(from: string, to: string) {
     return `
-      WITH full_licenses_lpn AS (
-        SELECT DISTINCT applicant_id
-        FROM public.ien_applicant_status_audit 
+       WITH active_applicants AS (
+        SELECT
+          t1.*
+        FROM (
+          SELECT 
+          applicants.id,
+          CASE 
+            WHEN 
+              COALESCE(
+                (
+                  SELECT ien_status.status_id 
+                  FROM public.ien_applicant_status_audit ien_status
+                  LEFT JOIN public.ien_applicant_status status ON status.id = ien_status.status_id
+                  WHERE
+                    ien_status.applicant_id = applicants.id AND ien_status.start_date::date <= '${to}' AND
+                    status.category IN ('${StatusCategory.LICENSING_REGISTRATION}', '${StatusCategory.RECRUITMENT}')
+                  ORDER BY ien_status.start_date DESC, ien_status.updated_date DESC
+                  LIMIT 1
+                ),
+                '${this.nil_uuid}'
+              ) IN ('${StatusId.WITHDREW_FROM_PROGRAM}', '${StatusId.JOB_OFFER_ACCEPTED}') 
+            THEN 0
+            ELSE 1 
+          END as active
+          FROM public.ien_applicants as applicants
+        ) as t1
+      ), full_licenses_lpn AS (
+        SELECT DISTINCT iasa.applicant_id
+        FROM public.ien_applicant_status_audit iasa 
+        INNER JOIN active_applicants aa ON aa.id = iasa.applicant_id
         WHERE 
-          status_id = '${StatusId.BCCNM_FULL_LICENCE_LPN}' AND
-          start_date <= '${to}' AND
-          start_date >= '${from}'
+          iasa.status_id = '${StatusId.BCCNM_FULL_LICENCE_LPN}' AND
+          iasa.start_date <= '${to}' AND
+          iasa.start_date >= '${from}' AND
+          aa.active = 1
       ),
       full_licenses_rn AS (
-        SELECT DISTINCT applicant_id
-        FROM public.ien_applicant_status_audit 
+        SELECT DISTINCT iasa.applicant_id
+        FROM public.ien_applicant_status_audit iasa
+        INNER JOIN active_applicants aa ON aa.id = iasa.applicant_id
         WHERE 
-          status_id = '${StatusId.BCCNM_FULL_LICENSE_RN}' AND
-          start_date <= '${to}' AND
-          start_date >= '${from}'
+          iasa.status_id = '${StatusId.BCCNM_FULL_LICENSE_RN}' AND
+          iasa.start_date <= '${to}' AND
+          iasa.start_date >= '${from}' AND
+          aa.active = 1
       ),
       partial_licenses_lpn AS (
         SELECT DISTINCT iasa.applicant_id
         FROM public.ien_applicant_status_audit iasa
+        INNER JOIN active_applicants aa ON aa.id = iasa.applicant_id
         WHERE 
           iasa.status_id = '${StatusId.BCCNM_PROVISIONAL_LICENSE_LPN}' AND
           iasa.start_date <= '${to}' AND
-          iasa.start_date >= '${from}'
+          iasa.start_date >= '${from}' AND
+          aa.active = 1
       ),
       partial_licenses_rn AS (
         SELECT DISTINCT iasa.applicant_id
         FROM public.ien_applicant_status_audit iasa
+        INNER JOIN active_applicants aa ON aa.id = iasa.applicant_id
         WHERE 
           iasa.status_id = '${StatusId.BCCNM_PROVISIONAL_LICENSE_RN}' AND
           iasa.start_date <= '${to}' AND
-          iasa.start_date >= '${from}'
+          iasa.start_date >= '${from}' AND
+          aa.active = 1
       )
       
       SELECT 'BCCNM Provisional Licence LPN' AS status, count(*) AS applicant_count FROM partial_licenses_lpn UNION ALL
@@ -379,13 +413,13 @@ export class ReportUtilService {
 
   applicantsInRecruitmentQuery(to: string) {
     return `
-      WITH withdew_hired_applicants AS (
+      WITH withdrew_hired_applicants AS (
         -- let's find candidates who were withdrew/hired in the process
         SELECT t1.* 
         FROM (
           SELECT
             ien_status.*,
-            ROW_NUMBER() OVER(PARTITION BY ien_status.applicant_id ORDER BY ien_status.start_date DESC) AS rank
+            ROW_NUMBER() OVER(PARTITION BY ien_status.applicant_id ORDER BY ien_status.start_date DESC, ien_status.updated_date DESC) AS rank
           FROM public.ien_applicant_status_audit ien_status
           LEFT JOIN public.ien_applicant_status status ON status.id=ien_status.status_id
           WHERE
@@ -403,12 +437,14 @@ export class ReportUtilService {
             wha.status_id,
             wha.job_id,
             wha.start_date,
-            CASE WHEN wha.status_id='${StatusId.WITHDREW_FROM_PROGRAM}' THEN (
+            CASE WHEN wha.status_id = '${StatusId.WITHDREW_FROM_PROGRAM}' THEN (
               SELECT iasa.start_date FROM public.ien_applicant_status_audit iasa 
-              WHERE iasa.applicant_id=wha.applicant_id AND iasa.status_id != '${StatusId.WITHDREW_FROM_PROGRAM}' AND iasa.start_date > wha.start_date
+              WHERE
+                iasa.applicant_id = wha.applicant_id AND iasa.status_id != '${StatusId.WITHDREW_FROM_PROGRAM}' AND
+                (iasa.start_date > wha.start_date OR (iasa.start_date = wha.start_date AND iasa.updated_date > wha.updated_date))
               ORDER BY start_date limit 1
             ) END as next_date
-          FROM withdew_hired_applicants wha
+          FROM withdrew_hired_applicants wha
           ) t1
         WHERE (t1.status_id='${StatusId.WITHDREW_FROM_PROGRAM}' and t1.next_date is null) OR t1.status_id='${StatusId.JOB_OFFER_ACCEPTED}'
       ),
@@ -421,7 +457,7 @@ export class ReportUtilService {
           (SELECT status_id FROM public.ien_applicant_status_audit as status 
             WHERE status.job_id=job.id AND start_date <= '${to}'
             -- put new status restriction here
-            ORDER BY start_date DESC limit 1) as status_id,
+            ORDER BY start_date DESC, updated_date DESC limit 1) as status_id,
           applicant_id
         FROM
           public.ien_applicant_jobs as job
@@ -539,7 +575,8 @@ export class ReportUtilService {
                   '${StatusId.RECEIVED_WORK_PERMIT}'
                   )
                 AND sa.start_date <= '${to}'
-              ORDER BY sa.start_date DESC limit 1
+              ORDER BY sa.start_date DESC, sa.updated_date DESC
+              LIMIT 1
             ) as status_id
           FROM hired_applicants as a1
         ) AS t1
@@ -624,8 +661,8 @@ export class ReportUtilService {
             SELECT aj.ha_pcn_id 
             FROM public.ien_applicant_status_audit asa JOIN public.ien_applicant_jobs aj on aj.id=asa.job_id
             WHERE arwp.applicant_id = asa.applicant_id
-            ORDER BY asa.start_date DESC
-            limit 1
+            ORDER BY asa.start_date DESC, asa.updated_date DESC
+            LIMIT 1
           ) as ha 
         FROM applicantReceivedWP arwp
       ), currentPeriod as (
@@ -714,7 +751,7 @@ export class ReportUtilService {
             iasa.status_id,
           iasa.job_id,
           iaj.ha_pcn_id as ha,
-              ROW_NUMBER() OVER(PARTITION BY iasa.applicant_id ORDER BY iasa.start_date DESC) AS rank
+              ROW_NUMBER() OVER(PARTITION BY iasa.applicant_id ORDER BY iasa.start_date DESC, iasa.updated_date DESC) AS rank
         FROM public.ien_applicant_status_audit iasa
         LEFT JOIN public.ien_applicant_jobs iaj ON iasa.job_id=iaj.id
         WHERE iasa.status_id IN ('${StatusId.WITHDREW_FROM_PROGRAM}', '${StatusId.JOB_OFFER_ACCEPTED}') AND iasa.start_date::date <= '${to}'
