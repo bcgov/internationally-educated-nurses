baf85da95ef1ad686b6e07dc8dfa8ee6f7c6ed8a
Cory Atwater <cory@freshworks.io>
2022-10-03 15:37:05 -0400

    add e2e test for report 1

---
+++ apps/api/test/reports.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+
+import { AppModule } from 'src/app.module';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { canActivate } from './override-guard';
+import dayjs from 'dayjs';
+import { validApplicantForReportOne } from './fixture/ien';
+import { randomUUID } from 'crypto';
+
+describe('Report Controller (e2e)', () => {
+  let app: INestApplication;
+  let applicanIdOne: string;
+  let applicanIdTwo: string;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate })
+      .compile();
+
+    applicanIdOne = randomUUID();
+    applicanIdTwo = randomUUID();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Add Applicant /ien (POST)', done => {
+    validApplicantForReportOne.applicant_id = applicanIdOne;
+    request(app.getHttpServer())
+      .post('/ien')
+      .send(validApplicantForReportOne)
+      .expect(201)
+      .end(done);
+  });
+
+  it('Add second Applicant /ien (POST)', done => {
+    validApplicantForReportOne.applicant_id = applicanIdTwo;
+    validApplicantForReportOne.last_name = 'notreport';
+    validApplicantForReportOne.email_address = 'test.report2@mailinator.com';
+    validApplicantForReportOne.registration_date = '2022-05-29';
+    request(app.getHttpServer())
+      .post('/ien')
+      .send(validApplicantForReportOne)
+      .expect(201)
+      .end(done);
+  });
+
+  it('Report 1 Summary - GET', done => {
+    const reportOneUrl = '/reports/applicant/registered';
+    const totalPeriods = Math.round(dayjs().diff(dayjs('2022-05-02'), 'day') / 28);
+
+    request(app.getHttpServer())
+      .get(reportOneUrl)
+      .expect(res => {
+        const { body } = res;
+
+        let total = 0;
+        body.forEach((app: any) => {
+          total += app.applicants;
+        });
+
+        expect(body.length).toBe(totalPeriods);
+        expect(total).toBe(2);
+        expect(body[0].applicants).toBe(1);
+        expect(body[1].applicants).toBe(1);
+      })
+      .expect(200)
+      .end(done);
+  });
+});
