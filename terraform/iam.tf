83e30fd30720e1a54b86ef8e6e4595ced5fe8d74
Cory Atwater <cory@freshworks.io>
2023-02-27 13:26:02 -0500

    remove bucket policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,31 +17,6 @@ resource "aws_iam_role" "lambda" {
   })
 }
 
-resource "aws_s3_bucket_policy" "api_lambda" {
-  bucket = aws_s3_bucket.api.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.api.arn}/*"
-        Principal = {
-          AWS = aws_iam_role.lambda.arn
-        }
-      },
-      {
-        Effect   = "Allow"
-        Action   = "s3:ListBucket"
-        Resource = aws_s3_bucket.api.arn
-        Principal = {
-          AWS = aws_iam_role.lambda.arn
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
