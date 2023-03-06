66604eb892b3815d70066a5a54e2b3761d2211f2
JGH-EY <James.Heading@ca.ey.com>
2023-03-06 11:13:24 -0400

    Cleaning up testing

--- apps/api/test/02-report-2.e2e-spec.ts
+++ apps/api/test/02-report-2.e2e-spec.ts
@@ -27,7 +27,6 @@ describe('Report 2 - Country of Education', () => {
 
     const { body: report } = await request(app.getHttpServer()).get(URLS.REPORT1);
     periods = report;
-    console.log(periods);
   });
 
   afterAll(async () => {
@@ -62,8 +61,7 @@ describe('Report 2 - Country of Education', () => {
     const country = applicant.nursing_educations[0].country as keyof typeof COUNTRY_OF_EDUCATIONS;
     const countBefore = periodBefore[COUNTRY_OF_EDUCATIONS[country]];
     const countAfter = periodAfter[COUNTRY_OF_EDUCATIONS[country]];
-    console.log(countBefore);
-    console.log(countAfter);
+
     expect(countAfter - countBefore).toBe(expectedDifference);
   };
 
@@ -78,14 +76,12 @@ describe('Report 2 - Country of Education', () => {
   it('Add applicant with education to the last period', async () => {
     const { from, to } = periods[periods.length - 1];
     let applicant: IENApplicantCreateUpdateDTO;
-    console.log('From:' + from + ' To:' + to);
     if (from === to) {
       applicant = getApplicant();
       applicant.registration_date = from;
     } else {
       applicant = getApplicant({ between: [from, to] });
     }
-    console.log(applicant);
     applicant.nursing_educations.push(getEducation());
     await testAddEducation(applicant, from, to);
   });
