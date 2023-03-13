51c8c8c95faf19d1618f8877b248e17e5d8bdcea
Cory Atwater <cory@freshworks.io>
2023-03-13 09:42:56 -0400

    make applicant_id col unique

--- apps/api/src/migration/1678711953274-ChangeApplicantIdColumnToVarChar.ts
+++ apps/api/src/migration/1678711953274-ChangeApplicantIdColumnToVarChar.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class ChangeApplicantIdColumnToVarChar1678711953274 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "ien_applicants" DROP COLUMN "applicant_id"`);
-    await queryRunner.query(`ALTER TABLE "ien_applicants" ADD "applicant_id" varchar`);
+    await queryRunner.query(`ALTER TABLE "ien_applicants" ADD "applicant_id" varchar UNIQUE`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
