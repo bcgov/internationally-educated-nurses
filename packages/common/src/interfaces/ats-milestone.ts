ad6aee255535bb5751addf2190affd8ba606fce6
Sunghwan Park <sunghwan.park@ca.ey.com>
2023-09-12 10:34:50 -0700

    IEN-873 refactor `applicant sync`

--- packages/common/src/interfaces/ats-milestone.ts
+++ packages/common/src/interfaces/ats-milestone.ts
@@ -3,4 +3,11 @@ export interface AtsMilestone {
   name: string;
   category: string;
   'process-related'?: boolean;
+  start_date?: string;
+  created_date?: string;
+  note?: any;
+  added_by?: number;
+  reason_id?: string;
+  reason_other?: string;
+  effective_date?: string;
 }
