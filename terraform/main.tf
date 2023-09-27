ca5f3e981948d2843ffc2d0e9d9d0f277ac870ef
David <david.bayly@ca.ey.com>
2023-09-27 17:58:59 -0600

    Updating assumed role for TF config

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region = var.region
   assume_role {
-    role_arn = "arn:aws:iam::${var.target_aws_account_id}:role/BCGOV_${var.target_env}_Automation_Admin_Role"
+    role_arn = "arn:aws:iam::${var.target_aws_account_id}:role/IEN_${var.target_env}_Automation_Admin_Role"
   }
 }
 
@@ -20,7 +20,7 @@ provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn = "arn:aws:iam::${var.target_aws_account_id}:role/BCGOV_${var.target_env}_Automation_Admin_Role"
+    role_arn = "arn:aws:iam::${var.target_aws_account_id}:role/IEN_${var.target_env}_Automation_Admin_Role"
   }
 }
 
