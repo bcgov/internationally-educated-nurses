d383c95a06e8571d88b35156a7c03405fadff4fa
David Bayly <david.bayly@ca.ey.com>
2024-06-26 11:37:07 -0600

    Shifting declaration outside switch statement

--- apps/api/src/syncdata.ts
+++ apps/api/src/syncdata.ts
@@ -18,6 +18,10 @@ export const handler: Handler = async (event, context: Context) => {
 
   logger.log(event, 'ATS-SYNC');
   logger.log(context, 'ATS-SYNC');
+  let from = undefined;
+  let to = undefined;
+  let page = undefined;
+  const regex = new RegExp(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])$/); //yyyy-mm-dd
 
   try {
     switch (event.path) {
@@ -27,10 +31,6 @@ export const handler: Handler = async (event, context: Context) => {
         break;
       case 'applicant-data':
         logger.log('Start applicant data import...', 'ATS-SYNC');
-        let from = undefined;
-        let to = undefined;
-        let page = undefined;
-        const regex = new RegExp(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])$/); //yyyy-mm-dd
         if (event.hasOwnProperty('from') && regex.test(event.from)) {
           from = event.from;
         } else {
