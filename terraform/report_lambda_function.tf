6e4a0336b75b70a1f8839f2b88f0ab3b3b5300af
Jerry Wang <jerryappleid761208@gmail.com>
2024-08-22 10:31:27 -0700

    add count[index]

--- terraform/report_lambda_function.tf
+++ terraform/report_lambda_function.tf
@@ -55,7 +55,7 @@ resource "aws_cloudwatch_event_rule" "cache_reports" {
 resource "aws_cloudwatch_event_target" "cache_reports" {
   ## Set count=0 to disable the cron job
   count = 0
-  rule  = aws_cloudwatch_event_rule.cache_reports.name
+  rule  = aws_cloudwatch_event_rule.cache_reports[count.index].name
   arn   = aws_lambda_function.CacheReports.arn
   input = "{\"path\": \"cache-reports\"}"
 }
@@ -67,5 +67,5 @@ resource "aws_lambda_permission" "cache_reports" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.CacheReports.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.cache_reports.arn
+  source_arn    = aws_cloudwatch_event_rule.cache_reports[count.index].arn
 }
