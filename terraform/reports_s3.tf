7c6ffadfcc498a071f086bdf531693a8653998c6
Jerry Wang <jerryappleid761208@gmail.com>
2024-08-23 14:49:47 -0700

    add tf cors

--- terraform/reports_s3.tf
+++ terraform/reports_s3.tf
@@ -1,3 +1,14 @@
 resource "aws_s3_bucket" "reports" {
   bucket = var.reports_bucket  
 }
+
+resource "aws_s3_bucket_cors_configuration" "reports_cors" {
+  bucket = aws_s3_bucket.reports.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"] // Replace with your frontend domain
+    expose_headers  = []
+  }
+}
