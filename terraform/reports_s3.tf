96dbe77b5b9233e1db77eda951a2a30399fc039d
Jerry Wang <jerryappleid761208@gmail.com>
2024-08-29 08:59:38 -0700

    fix pro url

--- terraform/reports_s3.tf
+++ terraform/reports_s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_cors_configuration" "reports_cors" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://dev.ien.freshworks.club", "https://test.ien.freshworks.club", "https://ien.freshworks.club"] // Replace with your frontend domain
+    allowed_origins = ["https://dev.ien.freshworks.club", "https://test.ien.freshworks.club", "https://ien.gov.bc.ca"] // Replace with your frontend domain
     expose_headers  = []
   }
 }
