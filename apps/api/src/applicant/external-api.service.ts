8be2b204e91a8d8e1d3be69dd000d641ec04bae9
David Bayly <54554766+dbayly-freshworks@users.noreply.github.com>
2024-09-03 14:24:57 -0600

    Fixed off by one error found in sync (#648)
    * Fixed off by one error found in sync
    
    * removed console.log, ran formatter

--- apps/api/src/applicant/external-api.service.ts
+++ apps/api/src/applicant/external-api.service.ts
@@ -284,6 +284,7 @@ export class ExternalAPIService {
 
     try {
       let applicants = await this.fetchApplicantsFromATS(from_date, to_date, page);
+
       if (!applicants) {
         return {
           done: true,
@@ -431,14 +432,13 @@ export class ExternalAPIService {
       return result;
     }
 
-    const applicants: any[] = await this.mapApplicants(data);
+    let applicants: any[] = await this.mapApplicants(data);
     const processed_applicants_list: any[] = [];
 
     for (let i = 0; i <= applicants?.length / 250; i++) {
-      const applicant_slice = applicants.slice(
-        i * 250, // Upsert applicants in chunks of 250
-        min([(i + 1) * 250, applicants.length - 1]), // Length of applicants array or the next 250 applicants, whichever is lower.
-      );
+      const start = i * 250; // Upsert applicants in chunks of 250
+      const end = min([(i + 1) * 250, applicants.length]); // Length of applicants array or the next 250 applicants, whichever is lower.
+      const applicant_slice = applicants.slice(start, end);
       const applicant_upsert_results = await manager.upsert(IENApplicant, applicant_slice, ['id']);
       result.applicants.processed += applicant_upsert_results?.identifiers?.length || 0;
       if (applicant_upsert_results.identifiers.length) {
