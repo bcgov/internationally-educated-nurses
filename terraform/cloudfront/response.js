7c847b1b766bbdf4237934478ac7d1ab3e65f070
Jerry Wang <jerryappleid761208@gmail.com>
2024-09-04 10:54:02 -0700

    test response

--- terraform/cloudfront/response.js
+++ terraform/cloudfront/response.js
@@ -1,6 +1,14 @@
+const crypto = require('crypto');
+
+function generateNonce() {
+  return crypto.randomBytes(16).toString('base64');
+}
+
 function handler(event) {
   var response = event.response;
   var headers = response.headers;
+  // Generate a nonce value
+  var nonce = generateNonce();
 
   // Set HTTP security headers
   // Since JavaScript doesn't allow for hyphens in variable names, we use the dict["key"] notation
@@ -8,7 +16,7 @@ function handler(event) {
   headers['content-security-policy'] = {
     // We need to hard code both as there isn't a good way of checking environment to dynamically determine which
     value:
-      "default-src 'self' https://keycloak.freshworks.club https://common-logon-dev.hlth.gov.bc.ca https://common-logon-test.hlth.gov.bc.ca https://common-logon.hlth.gov.bc.ca https://ien-dev-reports.s3.ca-central-1.amazonaws.com https://ien-test-reports.s3.ca-central-1.amazonaws.com https://ien-prod-reports.s3.ca-central-1.amazonaws.com; img-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; form-action 'self'; frame-ancestors 'self'",
+      "default-src 'self' https://keycloak.freshworks.club https://common-logon-dev.hlth.gov.bc.ca https://common-logon-test.hlth.gov.bc.ca https://common-logon.hlth.gov.bc.ca https://ien-dev-reports.s3.ca-central-1.amazonaws.com https://ien-test-reports.s3.ca-central-1.amazonaws.com https://ien-prod-reports.s3.ca-central-1.amazonaws.com; img-src 'self';  script-src 'self' 'nonce-" + nonce + "'; style-src 'self' 'nonce-" + nonce + "';" + "form-action 'self'; frame-ancestors 'self'",
   };
   headers['x-content-type-options'] = { value: 'nosniff' };
   headers['x-frame-options'] = { value: 'DENY' };
