2e32d42db754c5fbf58f082b787e03dc79acf23b
Jerry Wang <jerryappleid761208@gmail.com>
2024-09-04 11:58:11 -0700

    add cache react-select prettier

--- apps/web/src/components/CustomReactSelect.tsx
+++ apps/web/src/components/CustomReactSelect.tsx
@@ -1,12 +1,19 @@
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-import ReactSelect from 'react-select';
+import ReactSelect, { Props as ReactSelectProps } from 'react-select';
 import crypto from 'crypto';
 export * from 'react-select';
 
-export default function CustomReactSelect(props: any) {
+// Define a generic type for CustomReactSelect
+export default function CustomReactSelect<TOption, TIsMulti extends boolean = false>(
+  props: ReactSelectProps<TOption, TIsMulti>,
+) {
+  // Generate a nonce for CSP
   const nonce = crypto.randomBytes(16).toString('base64');
+
+  // Create Emotion cache with the generated nonce
   const cache = createCache({ key: 'css', prepend: true, nonce });
+
   return (
     <CacheProvider value={cache}>
       <ReactSelect {...props} />
