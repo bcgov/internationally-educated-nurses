74a36f956af89d00d07d7b0bd6a4dc5f23576255
Jerry Wang <jerryappleid761208@gmail.com>
2024-09-17 15:50:52 -0700

    add HEAD to s3 method

--- terraform/reports_s3.tf
+++ terraform/reports_s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_cors_configuration" "reports_cors" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
+    allowed_methods = ["GET", "HEAD"]
     allowed_origins = ["https://dev.ien.freshworks.club", "https://test.ien.freshworks.club", "https://ien.gov.bc.ca"] // Replace with your frontend domain
     expose_headers  = []
   }
